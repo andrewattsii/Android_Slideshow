dd88dbb5c48eb30ada03df0c8bac6b2eaf7326f4
Andre Watts <andrewattsii@gmail.com>
2020-02-12 12:25:19 -0500

    slideshow bug fixed

--- app/src/main/java/com/example/slideshowapp/MainActivity.kt
+++ app/src/main/java/com/example/slideshowapp/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
 
         //storing images in array
         var images = arrayOf(R.drawable.slide_1, R.drawable.slide_2, R.drawable.slide_3, R.drawable.slide_4 )
-
+        var index = 0
 
         //fetching the views to be sued in code , things to be clicked
         var imageSlide = findViewById<ImageView>(R.id.image_slide)
@@ -22,7 +22,15 @@ class MainActivity : AppCompatActivity() {
         var previous_btn = findViewById<Button>(R.id.previous_btn)
 
         //set onlcick listener
-        next_btn.setOnClickListener {  }
+        next_btn.setOnClickListener {
+            //when next button clicked will access image on app by default and call our images array and start at the first picture "[0]"
+            //each time it is clicked the index will have 1 added causing the index number to chang
+            if(index == images.size -1) {
+                index = 0
+            }
+            index = index + 1
+            imageSlide.setImageResource(images[index])
+        }
         previous_btn.setOnClickListener {  }
 
 
