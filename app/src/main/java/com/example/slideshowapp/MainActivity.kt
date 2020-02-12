29ce73edf7fb0e85d31cf02b3a6c368d09b26967
Andre Watts <andrewattsii@gmail.com>
2020-02-12 11:42:31 -0500

    start backend logic, accessing my views

--- app/src/main/java/com/example/slideshowapp/MainActivity.kt
+++ app/src/main/java/com/example/slideshowapp/MainActivity.kt
@@ -2,11 +2,29 @@ package com.example.slideshowapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        //storing images in array
+        var images = arrayOf(R.drawable.slide_1, R.drawable.slide_2, R.drawable.slide_3, R.drawable.slide_4 )
+
+
+        //fetching the views to be sued in code , things to be clicked
+        var imageSlide = findViewById<ImageView>(R.id.image_slide)
+        var next_btn = findViewById<Button>(R.id.next_btn)
+        var previous_btn = findViewById<Button>(R.id.previous_btn)
+
+        //set onlcick listener
+        next_btn.setOnClickListener {  }
+        previous_btn.setOnClickListener {  }
+
+
     }
 }
